22d3622f2ac5f91f77d197ea305fcd36b9640da1
Malachi S <malachis1447@gmail.com>
2023-08-14 09:00:32 +0000

    Added scripts 01-04

--- 01-HelloWorld/HelloWorld.cpp
+++ 01-HelloWorld/HelloWorld.cpp
@@ -1,3 +1,9 @@
+/* 
+ * HelloWorld.cpp
+ * Kain M. Snyder
+ * Mon Aug 14th 2023 0154 MDT
+*/
+
 #include <iostream>
 using namespace std;
 
---
+++ 02-DrawShape/DrawShape.cpp
@@ -0,0 +1,18 @@
+/* 
+ * DrawShape.cpp 
+ * Kain M. Snyder 
+ * Mon Aug 14th 2023 0157 MDT
+ */
+
+ #include <iostream>
+ using namespace std;
+
+ int main() {
+   
+    cout << "   /\\ \n ";
+    cout << " /  \\ \n";
+    cout << " /    \\ \n";
+    cout << "/______\\ \n";
+
+    return 0;
+ }
